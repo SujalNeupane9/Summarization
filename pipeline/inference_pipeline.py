2f055ad0f394b4f0ece7ceeeae6a26cddd271863
Sujal Neupane <121492848+SujalNeupane9@users.noreply.github.com>
2023-11-14 17:02:24 +0545

    Update inference_pipeline.py

--- pipeline/inference_pipeline.py
+++ pipeline/inference_pipeline.py
@@ -3,9 +3,9 @@ from utils.utils import tokenize_for_inference
 def infer_model(trainer):
     text = input("Enter the text you want to summarize: ")
     tokenized = tokenize_for_inference(text)
-    generated = trainer.model.generate(**tokenized, max_length=256)
+    generated = trainer.model.generate(tokenized, max_length=256)
     
     # Convert the generated output back to text
-    summary = trainer.tokenizer.decode(generated[0], skip_special_tokens=True)
+    summary = trainer.tokenizer.decode(generated.squeeze(), skip_special_tokens=True)
     
     return summary
