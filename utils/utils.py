78161c036fc96052ba126755cd56f1b6a5d77c41
Sujal Neupane <121492848+SujalNeupane9@users.noreply.github.com>
2023-11-14 17:08:02 +0545

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -4,10 +4,11 @@ model_nm = 't5-base'
 
 def tokenize_for_inference(text):
     tokenizer = AutoTokenizer.from_pretrained(model_nm)
-    model_inputs = tokenizer(
+    model_inputs = tokenizer.encode(
       text,
       max_length = 512,
       padding=True,
-      truncation=True
+      truncation=True,
+        return_tensors='pt'
     )
-    return model_inputs['input_ids']
+    return model_inputs
